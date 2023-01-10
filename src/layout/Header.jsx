1a3d6576b69f2ffe64cf307a7e1b002acdcc97ab
Sayesgareev <116744433+Sayesgareev@users.noreply.github.com>
2023-01-10 15:39:41 +0500

    project complete

---
+++ src/layout/Header.jsx
@@ -0,0 +1,21 @@
+function Header() {
+  return (
+    <div>
+      <nav className="blue-grey darken-4">
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">
+            React movie
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="#">Repo</a>
+            </li>
+            <li></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
